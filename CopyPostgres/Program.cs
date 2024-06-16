ebb805fd499007cf67d563e2467928d0c8ba25d9
anup1252000 <anup1252000@gmail.com>
2024-06-17 00:37:43 +0530

    Update Program.cs

--- CopyPostgres/Program.cs
+++ CopyPostgres/Program.cs
@@ -1,20 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
+// See https://aka.ms/new-console-template for more information
 using CopyPostgres;
-string connectionString = "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=yourpassword;";
-
-//List<Employee> employees = [];
-//for (int i = 0; i < 440000; i++)
-//{
-//    employees.Add(new Employee
-//    {
-//        Id = i,
-//        Name = "ganesh" + i
-//    });
-//}
-
-//BenchmarkRunner.Run<Helper>();
-Console.WriteLine($"started:{DateTime.Now}");
-await new Helper().BatchBooking(connectionString);
+Console.WriteLine($"started:{DateTime.Now.ToLocalTime()}");
+await new Helper().ParallelBinaryBulkInsertAsync();
 Console.WriteLine($"completed:{ DateTime.Now}");
 Console.ReadLine();
